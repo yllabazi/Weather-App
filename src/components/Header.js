605c4f9e91ac1d5300d28c6641ff3570e876e700
Ylli Abazi <ylli.abazi019@gmail.com>
2020-06-09 09:51:13 +0200

    Initial folder structure

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import Logo from '../elements/Logo';
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Logo />
+      <p>coded by yllabazi</p>
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/elements/Logo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+      <h1>WeatherApp</h1>
+  )
+}
+
+export default Logo;
