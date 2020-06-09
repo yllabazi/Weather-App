a065e744c65f8575eeeacaa87138aeb9136dccbe
Ylli Abazi <ylli.abazi019@gmail.com>
2020-06-09 17:43:27 +0200

    Add structure for weather data presentation

---
+++ src/components/Table.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const TabularData = () => {
+  return (
+    <div className='tabularData'>
+      <div className='image'>
+        <img id='weatherImg' src="#" alt="Current Weather" />
+      </div>
+      <div className='weatherData1'>2</div>
+      <div className='weatherData2'>3</div>
+      <div className='weatherData3'>4</div>
+      <div className='weatherData4'>5</div>
+    </div>
+  )
+}
+
+export default TabularData;
