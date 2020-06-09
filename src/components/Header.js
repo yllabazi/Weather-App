b10b2d2524d3f0e2affd6b441c77b5ca2957067d
Ylli Abazi <ylli.abazi019@gmail.com>
2020-06-09 10:08:44 +0200

    Add header with 'logo' and github link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import Logo from '../elements/Logo';
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faGithub} from '@fortawesome/free-brands-svg-icons';
 
 const Header = () => {
   return (
     <div className="header">
       <Logo />
-      <p>coded by yllabazi</p>
+      <a href="https://github.com/yllabazi" target="_blank"><p>coded by yllabazi <FontAwesomeIcon icon={faGithub}/></p></a>
     </div>
   )
 }
